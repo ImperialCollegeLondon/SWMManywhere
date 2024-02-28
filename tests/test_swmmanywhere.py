5e03873a5359a18cbe2a54fffa93fbca7e56d207
Dobson <bdobson@ic.ac.uk>
2024-02-28 13:01:18 +0000

    Function to run a swmm model

--- tests/test_swmmanywhere.py
+++ tests/test_swmmanywhere.py
@@ -1,7 +1,32 @@
 """Tests for the main module."""
-from swmmanywhere import __version__
+from pathlib import Path
+
+from swmmanywhere import __version__, swmmanywhere
 
 
 def test_version():
     """Check that the version is acceptable."""
     assert __version__ == "0.1.0"
+
+
+def test_run():
+    """Test the run function."""
+    model = Path(__file__).parent.parent / 'swmmanywhere' / 'defs' /\
+          'basic_drainage_all_bits.inp'
+    storevars = ['flooding','flow','runoff','depth']
+    results = swmmanywhere.run(model,
+                               reporting_iters = 50,
+                               storevars = storevars)
+    assert set(results.variable.unique()) == set(storevars)
+    
+    # Ensure more reporting iterations results in more results
+    results_ = swmmanywhere.run(model,
+                               reporting_iters = 25,
+                               storevars = storevars)
+    assert results_.shape[0] > results.shape[0]
+
+    # Ensure a shorter duration results in fewer results
+    results_ = swmmanywhere.run(model,
+                               duration = 10000,
+                               storevars = storevars)
+    assert results_.shape[0] < results.shape[0]
