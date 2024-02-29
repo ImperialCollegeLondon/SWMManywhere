1c829e9592d2165713720a14be009cf2f0e8ef74
Dobson <bdobson@ic.ac.uk>
2024-02-29 16:21:33 +0000

    Implement outlet matching
    -Update and test KStest
    -Add and test flow/flooding for a 'dominant' outlet

--- swmmanywhere/metric_utilities.py
+++ swmmanywhere/metric_utilities.py
@@ -113,6 +113,111 @@ class kstest_betweenness(BaseMetric):
         #TODO does it make more sense to use statistic or pvalue?
         return stats.ks_2samp(list(syn_betweenness.values()),
                               list(real_betweenness.values())).statistic
+    
+@register_metric
+class outlet_nse_flow(BaseMetric):
+    """Outlet NSE flow.
+
+    Calculate the Nash-Sutcliffe efficiency (NSE) of flow over time, where flow
+    is measured as the total flow of all arcs that drain to the 'dominant'
+    outlet node. The dominant outlet node of the 'real' network is calculated by
+    dominant_outlet, while the dominant outlet node of the 'synthetic' network
+    is calculated by best_outlet_match.
+    """
+    def __call__(self,
+                  synthetic_G: nx.Graph,
+                  synthetic_results: pd.DataFrame,
+                  real_G: nx.Graph,
+                  real_results: pd.DataFrame,
+                  real_subs: gpd.GeoDataFrame,
+                  *args,
+                  **kwargs) -> float:
+        """Run the evaluated metric."""
+        # Identify synthetic and real arcs that flow into the best outlet node
+        _, syn_outlet = best_outlet_match(synthetic_G, real_subs)
+        syn_arc = [d['id'] for u,v,d in synthetic_G.edges(data=True)
+                   if v == syn_outlet]
+        _, real_outlet = dominant_outlet(real_G, real_results)
+        real_arc = [d['id'] for u,v,d in real_G.edges(data=True)
+                    if v == real_outlet]
+        
+        # Extract flow data
+        syn_flow = synthetic_results.loc[(synthetic_results.variable == 'flow') &
+                                        (synthetic_results.object.isin(syn_arc))]
+        syn_flow = syn_flow.groupby('date').value.sum().reset_index()
+
+        real_flow = real_results.loc[(real_results.variable == 'flow') &
+                                        (real_results.object.isin(real_arc))]
+        real_flow = real_flow.groupby('date').value.sum().reset_index()
+
+        # Align data
+        df = pd.merge(syn_flow, 
+                      real_flow, 
+                      on = 'date', 
+                      suffixes = ('_syn','_real'),
+                      how = 'outer')
+        df = df.sort_values(by='date')
+
+        # Interpolate to time in real data
+        df['value_syn'] = df.set_index('date').value_syn.interpolate().values
+        df = df.dropna(subset = 'value_real')
+
+        # Calculate NSE
+        return nse(df.value_real, df.value_syn)
+
+@register_metric
+class outlet_nse_flooding(BaseMetric):
+    """Outlet NSE flooding.
+    
+    Calculate the Nash-Sutcliffe efficiency (NSE) of flooding over time, where
+    flooding is the total volume of flooded water across all nodes that drain
+    to the 'dominant' outlet node. The dominant outlet node of the 'real' 
+    network is calculated by dominant_outlet, while the dominant outlet node of
+    the 'synthetic' network is calculated by best_outlet_match.
+    """
+    def __call__(self,
+                  synthetic_G: nx.Graph,
+                  synthetic_results: pd.DataFrame,
+                  real_G: nx.Graph,
+                  real_results: pd.DataFrame,
+                  real_subs: gpd.GeoDataFrame,
+                  *args,
+                  **kwargs) -> float:
+        """Run the evaluated metric."""
+        # Identify synthetic and real outlet arcs
+        sg_syn, _ = best_outlet_match(synthetic_G, real_subs)
+        sg_real, _ = dominant_outlet(real_G, real_results)
+        
+        # Extract flooding data
+        syn_flood = synthetic_results.loc[(synthetic_results.variable == 'flooding') &
+                                        synthetic_results.object.isin(sg_syn.nodes)]
+        
+        real_flood = real_results.loc[(real_results.variable == 'flooding') &
+                                        real_results.object.isin(sg_real.nodes)]
+        
+        # Aggregate to total flooding over network
+        syn_flood = syn_flood.groupby('date').value.sum().reset_index()
+        real_flood = real_flood.groupby('date').value.sum().reset_index()
+
+        # Align data
+        df = pd.merge(syn_flood, 
+                      real_flood, 
+                      on = 'date', 
+                      suffixes = ('_syn','_real'),
+                      how = 'outer')
+        df = df.sort_values(by='date')
+
+        # Interpolate to time in real data
+        df['value_syn'] = df.set_index('date').value_syn.interpolate().values
+        df = df.dropna(subset='value_real')
+
+        # Calculate NSE
+        return nse(df.value_real, df.value_syn)
+
+def nse(y: np.ndarray,
+        yhat: np.ndarray) -> float:
+    """Calculate Nash-Sutcliffe efficiency (NSE)."""
+    return 1 - np.sum((y - yhat)**2) / np.sum((y - np.mean(y))**2)
 
 def best_outlet_match(synthetic_G: nx.Graph,
                       real_subs: gpd.GeoDataFrame) -> tuple[nx.Graph,int]:
--- tests/test_metric_utilities.py
+++ tests/test_metric_utilities.py
@@ -2,12 +2,49 @@ from pathlib import Path
 
 import geopandas as gpd
 import networkx as nx
+import numpy as np
 import pandas as pd
 import shapely
 
 from swmmanywhere import metric_utilities as mu
 from swmmanywhere.graph_utilities import load_graph
 
+
+def get_subs():
+    """Get a GeoDataFrame of subcatchments."""
+    subs = [shapely.Polygon([(700262, 5709928),
+                            (700262, 5709883),
+                            (700351, 5709883),
+                            (700351, 5709906),
+                            (700306, 5709906),
+                            (700306, 5709928),
+                            (700262, 5709928)]),
+            shapely.Polygon([(700306, 5709928),
+                            (700284, 5709928),
+                            (700284, 5709950),
+                            (700374, 5709950),
+                            (700374, 5709906),
+                            (700351, 5709906),
+                            (700306, 5709906),
+                            (700306, 5709928)]),
+            shapely.Polygon([(700351, 5709883),
+                            (700351, 5709906),
+                            (700374, 5709906),
+                            (700374, 5709883),
+                            (700396, 5709883),
+                            (700396, 5709816),
+                            (700329, 5709816),
+                            (700329, 5709838),
+                            (700329, 5709883),
+                            (700351, 5709883)])]
+
+    subs = gpd.GeoDataFrame(data = {'id' : [107733,
+                                            1696030874,
+                                            6277683849]
+                                            },
+                                    geometry = subs,
+                                    crs = 'EPSG:32630')
+    return subs
 
 def test_bias_flood_depth():
     """Test the bias_flood_depth metric."""
@@ -43,38 +80,7 @@ def test_bias_flood_depth():
 def test_best_outlet_match():
     """Test the best_outlet_match and ks_betweenness."""
     G = load_graph(Path(__file__).parent / 'test_data' / 'graph_topo_derived.json')
-    subs = [shapely.Polygon([(700262, 5709928),
-                            (700262, 5709883),
-                            (700351, 5709883),
-                            (700351, 5709906),
-                            (700306, 5709906),
-                            (700306, 5709928),
-                            (700262, 5709928)]),
-            shapely.Polygon([(700306, 5709928),
-                            (700284, 5709928),
-                            (700284, 5709950),
-                            (700374, 5709950),
-                            (700374, 5709906),
-                            (700351, 5709906),
-                            (700306, 5709906),
-                            (700306, 5709928)]),
-            shapely.Polygon([(700351, 5709883),
-                            (700351, 5709906),
-                            (700374, 5709906),
-                            (700374, 5709883),
-                            (700396, 5709883),
-                            (700396, 5709816),
-                            (700329, 5709816),
-                            (700329, 5709838),
-                            (700329, 5709883),
-                            (700351, 5709883)])]
-
-    subs = gpd.GeoDataFrame(data = {'id' : [107733,
-                                            1696030874,
-                                            6277683849]
-                                            },
-                                    geometry = subs,
-                                    crs = G.graph['crs'])
+    subs = get_subs()
     
     sg, outlet = mu.best_outlet_match(synthetic_G = G, 
                                      real_subs = subs)
@@ -108,3 +114,168 @@ def test_best_outlet_match():
                                 real_subs = subs,
                                 real_results = results)
     assert val == 0.4
+
+def test_nse():
+    """Test the nse metric."""
+    val = mu.nse(y = np.array([1,2,3,4,5]),
+                         yhat = np.array([1,2,3,4,5]))
+    assert val == 1.0
+
+    val = mu.nse(y = np.array([1,2,3,4,5]),
+                         yhat = np.array([3,3,3,3,3]))
+    assert val == 0.0
+
+def test_outlet_nse_flow():
+    """Test the outlet_nse_flow metric."""
+    # Load data
+    G = load_graph(Path(__file__).parent / 'test_data' / 'graph_topo_derived.json')
+    subs = get_subs()
+
+    # Mock results
+    results = pd.DataFrame([{'object' : 4253560,
+                             'variable' : 'flow',
+                             'value' : 10,
+                             'date' : pd.to_datetime('2021-01-01 00:00:00')},
+                            {'object' : '',
+                             'variable' : 'flow',
+                             'value' : 5,
+                             'date' : pd.to_datetime('2021-01-01 00:00:00')},
+                             {'object' : 4253560,
+                             'variable' : 'flow',
+                             'value' : 5,
+                             'date' : pd.to_datetime('2021-01-01 00:00:05')},
+                            {'object' : '',
+                             'variable' : 'flow',
+                             'value' : 2,
+                             'date' : pd.to_datetime('2021-01-01 00:00:05')}])
+    
+    # Calculate NSE (perfect results)
+    val = mu.metrics.outlet_nse_flow(synthetic_G = G,
+                                    synthetic_results = results,
+                                    real_G = G,
+                                    real_results = results,
+                                    real_subs = subs)
+    assert val == 1.0
+
+    # Calculate NSE (mean results)
+    results_ = results.copy()
+    results_.loc[[0,2],'value'] = 7.5
+    val = mu.metrics.outlet_nse_flow(synthetic_G = G,
+                                    synthetic_results = results_,
+                                    real_G = G,
+                                    real_results = results,
+                                    real_subs = subs)
+    assert val == 0.0
+
+    # Change the graph
+    G_ = G.copy()
+    new_outlet = list(G_.in_edges(12354833))[0][0]
+    nx.set_node_attributes(G_,
+                           new_outlet,
+                           'outlet')
+    G_.remove_node(12354833)
+    results_.loc[results_.object == 4253560, 'object'] = 725226531
+
+    # Calculate NSE (mean results)
+    val = mu.metrics.outlet_nse_flow(synthetic_G = G_,
+                                    synthetic_results = results_,
+                                    real_G = G,
+                                    real_results = results,
+                                    real_subs = subs)
+    assert val == 0.0
+
+    # Test time interpolation
+    results_.loc[2,'date'] = pd.to_datetime('2021-01-01 00:00:10')
+    results_.loc[[0,2], 'value'] = [0,30]
+    val = mu.metrics.outlet_nse_flow(synthetic_G = G_,
+                                    synthetic_results = results_,
+                                    real_G = G,
+                                    real_results = results,
+                                    real_subs = subs)
+    assert val == -15.0
+
+def test_outlet_nse_flooding():
+    """Test the outlet_nse_flow metric."""
+    # Load data
+    G = load_graph(Path(__file__).parent / 'test_data' / 'graph_topo_derived.json')
+    subs = get_subs()
+
+    # Mock results
+    results = pd.DataFrame([{'object' : 4253560,
+                             'variable' : 'flow',
+                             'value' : 10,
+                             'date' : pd.to_datetime('2021-01-01 00:00:00')},
+                             {'object' : 4253560,
+                             'variable' : 'flow',
+                             'value' : 5,
+                             'date' : pd.to_datetime('2021-01-01 00:00:05')},
+                             {'object' : 25472468,
+                             'variable' : 'flooding',
+                             'value' : 4.5,
+                             'date' : pd.to_datetime('2021-01-01 00:00:00')},
+                            {'object' : 770549936,
+                             'variable' : 'flooding',
+                             'value' : 5,
+                             'date' : pd.to_datetime('2021-01-01 00:00:00')},
+                            {'object' : 109753,
+                             'variable' : 'flooding',
+                             'value' : 10,
+                             'date' : pd.to_datetime('2021-01-01 00:00:00')},
+                             {'object' : 25472468,
+                             'variable' : 'flooding',
+                             'value' : 0,
+                             'date' : pd.to_datetime('2021-01-01 00:00:05')},
+                            {'object' : 770549936,
+                             'variable' : 'flooding',
+                             'value' : 5,
+                             'date' : pd.to_datetime('2021-01-01 00:00:05')},
+                            {'object' : 109753,
+                             'variable' : 'flooding',
+                             'value' : 15,
+                             'date' : pd.to_datetime('2021-01-01 00:00:05')}])
+    
+    # Calculate NSE (perfect results)
+    val = mu.metrics.outlet_nse_flooding(synthetic_G = G,
+                                    synthetic_results = results,
+                                    real_G = G,
+                                    real_results = results,
+                                    real_subs = subs)
+    assert val == 1.0
+
+    # Calculate NSE (mean results)
+    results_ = results.copy()
+    results_.loc[results_.object.isin([770549936, 25472468]),'value'] = [14.5 / 4] * 4
+    val = mu.metrics.outlet_nse_flooding(synthetic_G = G,
+                                    synthetic_results = results_,
+                                    real_G = G,
+                                    real_results = results,
+                                    real_subs = subs)
+    assert val == 0.0
+
+    # Change the graph
+    G_ = G.copy()
+    new_outlet = list(G_.in_edges(12354833))[0][0]
+    nx.set_node_attributes(G_,
+                           {x : new_outlet for x,d in G_.nodes(data = True) 
+                            if d['outlet'] == 12354833},
+                           'outlet')
+    G_.remove_node(12354833)
+
+    # Calculate NSE (mean results)
+    val = mu.metrics.outlet_nse_flooding(synthetic_G = G_,
+                                    synthetic_results = results_,
+                                    real_G = G,
+                                    real_results = results,
+                                    real_subs = subs)
+    assert val == 0.0
+
+    # Test time interpolation
+    results_.loc[results_.date == pd.to_datetime('2021-01-01 00:00:05'),
+                 'date'] = pd.to_datetime('2021-01-01 00:00:10')
+    
+    val = mu.metrics.outlet_nse_flooding(synthetic_G = G_,
+                                    synthetic_results = results_,
+                                    real_G = G,
+                                    real_results = results,
+                                    real_subs = subs)
+    assert val == 0.0
