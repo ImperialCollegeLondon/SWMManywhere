62457a8e55e97a57588c6553113b86adc4b46922
Dobson <bdobson@ic.ac.uk>
2024-03-05 13:59:41 +0000

    Test global behaviour of logger

--- swmmanywhere/utils.py
+++ swmmanywhere/utils.py
@@ -3,13 +3,12 @@
 
 @author: Barney
 """
-import os
 import sys
 
 import loguru
 
 
-def get_logger(verbose: bool = False) -> loguru.logger:
+def get_logger() -> loguru.logger:
     """Get a logger."""
     logger = loguru.logger
     logger.configure(
@@ -26,10 +25,9 @@ def get_logger(verbose: bool = False) -> loguru.logger:
             }
         ]
     )
-    if os.getenv("PACKAGE_NAME_VERBOSE", str(verbose)).lower() == "true":
-        logger.enable("package_name")
-    else:
-        logger.disable("package_name")
+    # Disable by default
+    logger.disable("swmmanywhere")
     return logger
 
 logger = get_logger()
+logger.test_logger = lambda : logger.info("This is a test message.")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,24 +10,47 @@ from swmmanywhere.utils import logger
 
 
 def test_logger():
-    """Test the get_logger function."""
+    """Test logger."""
+    logger.enable('swmmanywhere')
     assert logger is not None
+    logger.test_logger()
     logger.debug("This is a debug message.")
     logger.info("This is an info message.")
     logger.warning("This is a warning message.")
     logger.error("This is an error message.")
     logger.critical("This is a critical message.")
-
     with NamedTemporaryFile(suffix='.log',
                             mode = 'w+b',
                             delete=False) as temp_file:
         fid = Path(temp_file.name)
         logger.add(fid)
-        logger.debug("This is a debug message.")
-        logger.info("This is an info message.")
-        logger.warning("This is a warning message.")
-        logger.error("This is an error message.")
-        logger.critical("This is a critical message.")
+        logger.test_logger()
         assert temp_file.read() != b""
+        logger.remove()
+    fid.unlink()
+
+def test_logger_disable():    
+    """Test the disable function."""
+    with NamedTemporaryFile(suffix='.log',
+                            mode = 'w+b',
+                            delete=False) as temp_file:
+        fid = Path(temp_file.name)
+        logger.disable('swmmanywhere')
+        logger.add(fid)
+        logger.test_logger()
+        assert temp_file.read() == b""
+        logger.remove()
+    fid.unlink()
+
+def test_logger_reimport():
+    """Reimport logger to check that changes from disable are persistent."""
+    from swmmanywhere.utils import logger
+    with NamedTemporaryFile(suffix='.log',
+                            mode = 'w+b',
+                            delete=False) as temp_file:    
+        fid = Path(temp_file.name)
+        logger.add(fid)
+        logger.test_logger()
+        assert temp_file.read() == b""
         logger.remove()
     fid.unlink()
