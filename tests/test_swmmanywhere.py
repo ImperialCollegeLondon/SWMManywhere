e68530ea7abbe2cc63cdcdf57b67759f5c9e08b6
barneydobson <barnaby.dobson1@gmail.com>
2024-03-16 13:45:42 +0000

    Update test_swmmanywhere.py

--- tests/test_swmmanywhere.py
+++ tests/test_swmmanywhere.py
@@ -133,3 +133,33 @@ def test_load_config_schema_validation():
             swmmanywhere.load_config(base_dir / 'test_config.yml')
             assert "null" in str(exc_info.value)
 
+def test_check_parameters_to_sample():
+    """Test the check_parameters_to_sample validation."""
+    with tempfile.TemporaryDirectory() as temp_dir:
+        test_data_dir = Path(__file__).parent / 'test_data'
+        defs_dir = Path(__file__).parent.parent / 'swmmanywhere' / 'defs'
+        base_dir = Path(temp_dir)
+
+        # Load the config
+        with (test_data_dir / 'demo_config.yml').open('r') as f:
+            config = yaml.safe_load(f)
+        
+        # Correct and avoid filevalidation errors
+        config['real'] = None
+        
+        # Fill with unused paths to avoid filevalidation errors
+        config['base_dir'] = str(defs_dir / 'storm.dat')
+        config['api_keys'] = str(defs_dir / 'storm.dat')
+
+        # Make an edit not to schema
+        config['parameters_to_sample'] = ['not_a_parameter']
+        
+        with open(base_dir / 'test_config.yml', 'w') as f:
+            yaml.dump(config, f)
+
+        # Test schema validation
+        with pytest.raises(ValueError) as exc_info:
+            swmmanywhere.load_config(base_dir / 'test_config.yml')
+            assert "not_a_parameter" in str(exc_info.value)
+
+
