23c909c1168002c31eb11d03c3a73cecde6a2904
barneydobson <barnaby.dobson1@gmail.com>
2024-05-28 16:04:29 +0100

    Update logging.py

--- swmmanywhere/logging.py
+++ swmmanywhere/logging.py
@@ -4,7 +4,7 @@ Example:
 >>> import os
 >>> os.environ["SWMMANYWHERE_VERBOSE"] = "true" 
 >>> # logging is now enabled in any swmmanywhere module
->>> from swmmanywhere.custom_logging import logger # You can now log yourself
+>>> from swmmanywhere.logging import logger # You can now log yourself
 >>> logger.info("This is an info message.") # Write to stdout # doctest: +SKIP
 This is an info message.
 >>> logger.add("file.log") # Add a log file # doctest: +SKIP
