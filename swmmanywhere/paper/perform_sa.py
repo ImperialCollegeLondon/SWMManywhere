add heatmap and correct group calculation

swmmanywhere/paper/perform_sa.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 from SALib.analyze import sobol
 from tqdm import tqdm
@@ -19,9 +18,9 @@ from swmmanywhere.swmmanywhere import load_config
 # %%
 # Load the configuration file and extract relevant data
 if __name__ == 'main':
-    project = 'cranbrook'
+    project = 'cranbrook_formatted'
     base_dir = Path.home() / "Documents" / "data" / "swmmanywhere"
-    config_path = base_dir / project / f'{project}_hpc.yml'
+    config_path = base_dir / project / 'cf.yml'
     config = load_config(config_path, validation = False)
     config['base_dir'] = base_dir / project
     objectives = config['metric_list']
@@ -39,12 +38,15 @@ if __name__ == 'main':
     # Concatenate the results
     df = pd.concat(dfs)
 
-    # Log deltacon0 because it can be extremely large
-    df['nc_deltacon0'] = np.log(df['nc_deltacon0'])
+    df = df.loc[:,~df.columns.str.contains('subcatchment')]
+    df = df.drop('bias_flood_depth',axis=1)
+
     df = df.sort_values(by = 'iter')
 
+    objectives = set(objectives).intersection(df.columns)
+
     # Make a directory to store plots in
-    plot_fid = results_dir / 'plots'
+    plot_fid = results_dir.parent / 'plots'
     plot_fid.mkdir(exist_ok=True, parents=True)
 
     # %% [markdown]
@@ -79,12 +81,15 @@ if __name__ == 'main':
 
     # Perform the sensitivity analysis for groups
     problem['outputs'] = objectives
-    rg = {objective: sobol.analyze(problem, 
-                        df[objective].iloc[0:
-                                            (2**(config['sample_magnitude'] + 1) * 10)]
-                                            .values,
-                        print_to_console=False) 
-                        for objective in objectives}
+    rg = {objective: sobol.analyze(
+                problem, 
+                df[objective].iloc[0:
+                                    (2**(config['sample_magnitude'] + 1) *\
+                                      (len(set(problem['groups'])) + 1))]
+                                    .values,
+                print_to_console=False
+            ) 
+            for objective in objectives}
 
     # Perform the sensitivity analysis for parameters
     problemi = problem.copy()
@@ -99,3 +104,7 @@ if __name__ == 'main':
         swplt.plot_sensitivity_indices(r_, 
                                      objectives, 
                                      plot_fid / f'{groups}_indices.png')
+
+    # Heatmap of sensitivity indices    
+    for r_, groups in zip([rg,ri],  ['groups','parameters']):
+        swplt.heatmaps(r_, plot_fid / f'heatmap_{groups}_indices.png')