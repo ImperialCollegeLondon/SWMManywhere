51b574479e78111bcec5296c2dc5bffb0e374386
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-09-20 06:26:40 +0100

    Add more classifiers and hatch-vcs

--- swmmanywhere/__init__.py
+++ swmmanywhere/__init__.py
@@ -1,7 +1,12 @@
 """The main module for MyProject."""
 from __future__ import annotations
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("swmmanywhere")
+except PackageNotFoundError:
+    __version__ = "999"
 
 # Importing module to register the graphfcns and made them available
 from . import graphfcns  # noqa: F401
