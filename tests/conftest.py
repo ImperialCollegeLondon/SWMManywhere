fcfad4b45604b33939a2dee7593f3b93dcfec6e0
barneydobson <barnaby.dobson1@gmail.com>
2025-04-03 15:18:10 +0100

    precomit

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ def pytest_collection_modifyitems(config, items):
 @pytest.fixture
 def wbt_path() -> str:
     """Determine the platform specific binary for whiteboxtools for testing.
-    
+
     All WBT binaries are stored in `wbt_zip` within the `tests` directory.
 
     Based on implementation in [`pywbt`](https://github.com/cheginit/pywbt).
